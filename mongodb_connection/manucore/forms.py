b814d04917029cb3dda3447a6b2667cd3c6c4ed1
Hafzi Hammadi <225443036@mhs.polman-bandung.ac.id>
2026-05-06 18:34:04 +0700

    Delete mongodb_connection directory
    mo dipindahin

--- mongodb_connection/manucore/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-class RegisterForm(forms.Form):
-    nama_depan = forms.CharField(max_length=100)
-    nama_belakang = forms.CharField(max_length=100)
-    email = forms.EmailField()
-    company_name = forms.CharField(max_length=200)
-    password = forms.CharField(widget=forms.PasswordInput)
--- mongodb_connection/mongodb_connection/db.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient("mongodb+srv://manucore:banggajadianakpolman@workordermanagement.pzqc53x.mongodb.net/")
-db = client["manucore_db"]
-
-clients_collection = db["client"]
-users_collection = db["staff_users"]
